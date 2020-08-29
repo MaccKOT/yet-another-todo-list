f5683e6c26126deb248980372c163c2cc23ea291
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2020-08-29 13:44:26 +0400

    Form works with hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+//import components
 import Header from './components/Header';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+
   return (
     <div>
       <Header />
-      <Form />
+      <Form setInputText={setInputText} />
       <TodoList />
     </div>
   );
